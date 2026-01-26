bc861f3dd06b4740a7bd01bf39816794862df02d
aleks-byte770 <aleks09102006@gmail.com>
2026-01-26 10:03:23 +0500

    Add files via upload

--- vite.config.ts
+++ vite.config.ts
@@ -1,26 +1,30 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import path from "path";
+import { fileURLToPath } from "url";
+import { dirname, resolve } from "path";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "./src"),
-      "@store": path.resolve(__dirname, "./src/store"),
-      "@store/": path.resolve(__dirname, "./src/store") + "/",
-      "@components": path.resolve(__dirname, "./src/components"),
-      "@components/": path.resolve(__dirname, "./src/components") + "/",
-      "@pages": path.resolve(__dirname, "./src/pages"),
-      "@pages/": path.resolve(__dirname, "./src/pages") + "/",
-      "@hooks": path.resolve(__dirname, "./src/hooks"),
-      "@hooks/": path.resolve(__dirname, "./src/hooks") + "/",
-      "@api": path.resolve(__dirname, "./src/api"),
-      "@api/": path.resolve(__dirname, "./src/api") + "/",
-      "@types": path.resolve(__dirname, "./src/types"),
-      "@types/": path.resolve(__dirname, "./src/types") + "/",
-      "@styles": path.resolve(__dirname, "./src/styles"),
-      "@styles/": path.resolve(__dirname, "./src/styles") + "/",
+      "@": resolve(__dirname, "./src"),
+      "@store": resolve(__dirname, "./src/store"),
+      "@store/": resolve(__dirname, "./src/store") + "/",
+      "@components": resolve(__dirname, "./src/components"),
+      "@components/": resolve(__dirname, "./src/components") + "/",
+      "@pages": resolve(__dirname, "./src/pages"),
+      "@pages/": resolve(__dirname, "./src/pages") + "/",
+      "@hooks": resolve(__dirname, "./src/hooks"),
+      "@hooks/": resolve(__dirname, "./src/hooks") + "/",
+      "@api": resolve(__dirname, "./src/api"),
+      "@api/": resolve(__dirname, "./src/api") + "/",
+      "@types": resolve(__dirname, "./src/types"),
+      "@types/": resolve(__dirname, "./src/types") + "/",
+      "@styles": resolve(__dirname, "./src/styles"),
+      "@styles/": resolve(__dirname, "./src/styles") + "/",
     },
   },
   server: {
